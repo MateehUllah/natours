8f7944a28aac8e5396f64e5c6b226b871b1a0007
Mateeh Ullah <mateehullah89@gmail.com>
2022-07-31 03:04:55 +0500

    Improved stripe booking

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const server = app.listen(port, () => {
 //Test
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLER REJECTION\n shutting down....');
+  console.log(err);
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
